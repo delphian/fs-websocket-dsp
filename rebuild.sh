e1e08ba691e7a67f0dc9d66a9d1c9d103b1f1295
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2021-04-03 23:52:41 -0700

    Added rebuild script. Test of default echo websocket protocol

---
+++ rebuild.sh
@@ -0,0 +1,1 @@
+rm -Rf build && mkdir build && cd build && cmake .. -DCMAKE_BUILD_TYPE=Debug && cd ..
---
+++ server.c
@@ -0,0 +1,142 @@
+/* File name is hello_world.c */
+#include <stdio.h>
+#include <libwebsockets.h>
+#include <string.h>
+#include <signal.h>
+#include <complex.h>
+#include <liquid/liquid.h>
+
+#define LWS_PLUGIN_STATIC
+#include "protocol_echo.c"
+
+
+
+int interpolator() {
+    unsigned int M  = 4;     // interpolation factor
+    unsigned int m  = 12;    // filter delay [symbols]
+    float        As = 60.0f; // filter stop-band attenuation [dB]
+
+    // create interpolator from prototype
+    firinterp_crcf interp = firinterp_crcf_create_kaiser(M,m,As);
+    float complex x = 1.0f + 0.5f * I;  // input sample
+    float complex y[M];      // interpolated output buffer
+
+    // repeat on input sample data as needed
+    {
+        firinterp_crcf_execute(interp, x, y);
+    }
+
+    // destroy interpolator object
+    firinterp_crcf_destroy(interp);
+
+	int i;
+	for (i = 0; i < M; i++) {
+		printf("%f + i%f\n", creal(y[i]), cimag(y[i]));
+	}
+
+    return 0;
+}
+
+
+
+static struct lws_protocols protocols[] = {
+	LWS_PLUGIN_PROTOCOL_MINIMAL_SERVER_ECHO,
+	{ NULL, NULL, 0, 0 } /* terminator */
+};
+
+static int interrupted, port = 7681, options;
+
+/* pass pointers to shared vars to the protocol */
+
+static const struct lws_protocol_vhost_options pvo_options = {
+	NULL,
+	NULL,
+	"options",		/* pvo name */
+	(void *)&options	/* pvo value */
+};
+
+static const struct lws_protocol_vhost_options pvo_interrupted = {
+	&pvo_options,
+	NULL,
+	"interrupted",		/* pvo name */
+	(void *)&interrupted	/* pvo value */
+};
+
+static const struct lws_protocol_vhost_options pvo = {
+	NULL,				/* "next" pvo linked-list */
+	&pvo_interrupted,		/* "child" pvo linked-list */
+	"lws-minimal-server-echo",	/* protocol name we belong to on this vhost */
+	""				/* ignored */
+};
+static const struct lws_extension extensions[] = {
+	{
+		"permessage-deflate",
+		lws_extension_callback_pm_deflate,
+		"permessage-deflate"
+		 "; client_no_context_takeover"
+		 "; client_max_window_bits"
+	},
+	{ NULL, NULL, NULL /* terminator */ }
+};
+
+void sigint_handler(int sig)
+{
+	interrupted = 1;
+}
+
+int main(int argc, const char **argv)
+{
+	struct lws_context_creation_info info;
+	struct lws_context *context;
+	const char *p;
+	int n = 0, logs = LLL_USER | LLL_ERR | LLL_WARN | LLL_NOTICE
+			/* for LLL_ verbosity above NOTICE to be built into lws,
+			 * lws must have been configured and built with
+			 * -DCMAKE_BUILD_TYPE=DEBUG instead of =RELEASE */
+			/* | LLL_INFO */ /* | LLL_PARSER */ /* | LLL_HEADER */
+			/* | LLL_EXT */ /* | LLL_CLIENT */ /* | LLL_LATENCY */
+			/* | LLL_DEBUG */;
+
+	signal(SIGINT, sigint_handler);
+
+	if ((p = lws_cmdline_option(argc, argv, "-d")))
+		logs = atoi(p);
+
+	lws_set_log_level(logs, NULL);
+	lwsl_user("LWS minimal ws client echo + permessage-deflate + multifragment bulk message\n");
+	lwsl_user("   lws-minimal-ws-client-echo [-n (no exts)] [-p port] [-o (once)]\n");
+
+
+	if ((p = lws_cmdline_option(argc, argv, "-p")))
+		port = atoi(p);
+
+	if (lws_cmdline_option(argc, argv, "-o"))
+		options |= 1;
+
+	memset(&info, 0, sizeof info); /* otherwise uninitialized garbage */
+	info.port = port;
+	info.protocols = protocols;
+	info.pvo = &pvo;
+	if (!lws_cmdline_option(argc, argv, "-n"))
+		info.extensions = extensions;
+	info.pt_serv_buf_size = 32 * 1024;
+	info.options = LWS_SERVER_OPTION_VALIDATE_UTF8 |
+		LWS_SERVER_OPTION_HTTP_HEADERS_SECURITY_BEST_PRACTICES_ENFORCE;
+
+	context = lws_create_context(&info);
+	if (!context) {
+		lwsl_err("lws init failed\n");
+		return 1;
+	}
+
+	while (n >= 0 && !interrupted)
+		n = lws_service(context, 0);
+
+	lws_context_destroy(context);
+
+	lwsl_user("Completed %s\n", interrupted == 2 ? "OK" : "failed");
+
+	return interrupted != 2;
+}
+
+
